fb311659542fb09d80ef811b75e87d0f90df2789
阿樊提 <fanjiahe2000@163.com>
2015-07-12 09:45:08 +0800

    Update Mage.php

--- app/Mage.php
+++ app/Mage.php
@@ -1,35 +1,8 @@
 <?php
-/**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category   Mage
- * @package    Mage_Core
- * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-define('DS', DIRECTORY_SEPARATOR);
+define('DS', DIRECTORY_SEPARATOR);//使用简写
 define('PS', PATH_SEPARATOR);
-define('BP', dirname(dirname(__FILE__)));
-
-Mage::register('original_include_path', get_include_path());
-
+define('BP', dirname(dirname(__FILE__)));//business process的目录
+Mage::register('original_include_path', get_include_path());//获得include的path列表
 if (defined('COMPILER_INCLUDE_PATH')) {
     $appPath = COMPILER_INCLUDE_PATH;
     set_include_path($appPath . PS . Mage::registry('original_include_path'));
